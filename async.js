83c379d094f9f76b73ed23dcc6a41d768ba00f01
Dimplereddy28 <Dimplereddy.yalamuri@gmail.com>
2025-01-22 12:06:05 +0530

    pushing async files

---
+++ async.js
@@ -0,0 +1,16 @@
+/*console.log(10);
+console.log(20);
+setTimeout(()=>{
+    console.log(30);
+    // setTimeout method is creating a subtask
+},(500))
+console.log(40);
+console.log(50);*/
+
+console.log(10);
+console.log(20);
+setInterval(()=>{
+    console.log(30);
+},1500)
+console.log(40);
+console.log(50);
