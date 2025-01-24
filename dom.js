b82a9612154b7a6bfa6ed796dd2a6fcf6a05f7e6
Dimplereddy28 <Dimplereddy.yalamuri@gmail.com>
2025-01-24 21:18:16 +0530

    Push files

--- dom.js
+++ dom.js
@@ -5,7 +5,21 @@
 // element.innerText = "G+"
 // console.log(element);
 
-let ele = document.getElementById("test");
+/*let ele = document.getElementById("test");
 // ele.innerText= "<h1>Header</h1>"
 ele.innerHTML = "<h1>Header</h1>"
+console.log(ele);8*/
+
+/*let ele = document.getElementsByClassName("test");
 console.log(ele);
+console.log(Array.isArray(ele));
+ele[0].innerText="Hello"
+
+let x = [...ele];
+x.map(element=>{
+    element.style.backgroundColor="orange"
+})*/
+
+let ele = document.querySelectorAll(".test");
+console.log(ele);
+
