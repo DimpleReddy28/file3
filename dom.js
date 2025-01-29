9916683a9a7c8b359c757beeca2edf8e18b279b5
Dimplereddy28 <Dimplereddy.yalamuri@gmail.com>
2025-01-29 12:01:44 +0530

    pushing files into git

--- dom.js
+++ dom.js
@@ -32,7 +32,7 @@ console.log(image);
 document.body.appendChild(ele);
 document.body.appendChild(image);*/
 
-let form = document.querySelector("form");
+/*let form = document.querySelector("form");
 let username = document.getElementById("uName");
 let mail = document.getElementById("uMail");
 let psw = document.getElementById("uPass");
@@ -42,10 +42,93 @@ form.addEventListener("submit",(event)=>{
     let uname = username.value;
     let uemail = mail.value;
     let upass = psw.value;
-  let userDetails={
-    uname , uemail,upass
+    let userDetails={
+        uname , uemail,upass
     }
     console.log(userDetails);
     localStorage.setItem("userData" , JSON.stringify(userDetails))
 
+})
+
+let mainEle = document.createElement("div");
+mainEle.setAttribute("id","mainBlock");
+mainEle.style.border="2px solid red";
+mainEle.style.width="550px";
+mainEle.style.height="550px";
+
+console.log(mainEle);
+
+let topEle = document.createElement("div");
+topEle.setAttribute("class","topBlock");
+
+let image = document.createElement("img");
+img.src=""C:\Users\GangiReddy\OneDrive\Pictures\71qTm-Xrh0L.jpg"";
+image.style.width="550px"
+
+let bottomEle = document.createElement("div");
+botttomEle.setAttribute("class","bottomBlock");
+
+let h1 = document.createElement("h1");
+h1.innerText = "CAT";
+h1.style.textAlign = "center"
+
+let btn = document.createElement("button");
+btn.innerText="View More";
+btn.style.border = "none";
+btn.style.padding = "20px";
+btn.style.backgroundColor = "dodgerblue";
+btn.style.color = "white";
+
+bottomEle.appendChild(h1);
+bottomEle.appendChild(btn);
+
+topEle.appendChild(image);
+mainele.appendchild(topEle);
+mainEle.appendChild(bottomEle);
+document.body.appendChild(mainEle); */
+
+let form = document.querySelector("form");
+
+let username = document.getElementById("uName");
+let password = document.getElementById("uPass");
+let check = document.getElementById("check");
+let show = document.getElementById("show")
+
+let gender = document.getElementsByName("gender");
+//console.log(gender);   
+
+cj=heck.addEventListener("click",event=>{
+    if(event.target.checked == true){
+        password.setAttribute("type","text");
+        show.innerText = "hide password";
+    }
+    else{
+        password.setAttribute("type","password");
+            show.innerText = "show password";
+    }
+})
+
+form.addEventListener("submit" , event=>{
+    event.preventDefault();
+    let un = username.value;
+    let up = password.value;
+    let gen="";
+
+    for(let i=0;i<=gender.length-1;i++){
+        //console.log(gender[i].value)
+        //console.log(gender[i].checked);
+        if(gender[i].checked==true){
+            //console.log(gender[i].value)
+            gen=gender[i].value;
+        }
+    }
+
+    //console.log(un , up , gen);
+    let userDetails={
+        username : un,
+        password : up,
+        gender : gen 
+    }
+    console.log(userDetails);
+    sessionStorage.setItem("userData",JSON.stringify(userDetails));
 })
